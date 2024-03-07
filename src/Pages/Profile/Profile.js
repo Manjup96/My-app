f268d32c1d03a22eb0f5f0eb1d913ba0638c2d2f
manju prasad <manjuprasad.4327@gmail.com>
2024-03-07 14:08:20 +0530

    Manju

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { useAuth } from '../../context/AuthContext';
-import Sidebar from '../../shared/Sidebar';
-import UserDetails from './UserDetails'
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import Sidebar from "../../shared/Sidebar";
+import UserDetails from "./UserDetails";
 const Profile = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
   return (
     <div>
-        <div className="container-fluid">
-
-            <div className="row" >
-                <div className="col-md-3">
-                    <Sidebar />
-                </div>
-                <div className="col-md-9">
-                <UserDetails />
-                </div>
-            </div>
-       
-            </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <UserDetails />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/shared/Sidebar.js
+++ src/shared/Sidebar.js
@@ -1,44 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+import React, { useState } from "react";
+import '../styles/components/Sidebar.scss';
+import { NavLink } from "react-router-dom"; 
+import { FaBars, FaHome, FaUserAlt, FaUtensils, FaNewspaper, FaExclamationCircle } from "react-icons/fa";
+import logo from "../Asset/images/logo.png";
+ import Header from "../shared/Header";
+
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <FaHome />,
+    },
+    {
+     
+      path: "/meals",
+      name: "Meals",
+      icon: <FaUtensils />,
+    },
+    {
+      
+      path: "/news",
+      name: "News",
+      icon: <FaNewspaper />,
+    },
+    {
+      
+      path: "/complaints",
+      name: "Complaints",
+      icon: <FaExclamationCircle />,
+    },
+    {
+         path: "/profile",
+       name: "My Profile",
+        icon: <FaUserAlt />,
+    },
+   
+  ];
+
   return (
-    <nav className="col-md-2 d-md-block bg-light sidebar">
-      <div className="position-sticky">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/meals">
-              Meals
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/news">
-              News
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/complaints">
-            Complaints
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/payments">
-            Payments
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/profile">
-              Profile
-            </Link>
-          </li>
-        </ul>
+    <div className="container">
+      <Header /> 
+      <div style={{ width: isOpen ? "250px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+          <img src={logo} alt="Logo" style={{ width: '100px', height: '100px' }} />
+      <h4>PG Tenant</h4>
+          </h1>
+          <div className="bars">
+            <FaBars
+              className={isOpen ? "toggle-btn open" : "toggle-btn"}
+              onClick={toggleSidebar}
+            />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeClassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
       </div>
-    </nav>
+      <main>{children}</main>
+    </div>
   );
 };
 
